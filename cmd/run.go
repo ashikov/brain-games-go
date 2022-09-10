941ca03a889bc2ca73c574fedb63eaca4af2ea7c
Roman Ashikov <pickle@ashikov.ru>
2022-09-10 22:51:22 +0700

    refactoring
    remove comments
    change beckticks to quotes

--- cmd/run.go
+++ cmd/run.go
@@ -6,11 +6,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// runCmd represents the run command
 var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Starts the game",
-	Long: `Starts the game passed as an argument`,
+	Long: "Starts the game passed as an argument",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("run called")
 	},
@@ -18,14 +17,4 @@ var runCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(runCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
